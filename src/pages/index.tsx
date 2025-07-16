a5fc1de8cf2fa65487f51151930e5912225cc3d6
dnainform <dnainform@gmail.com>
2025-07-16 12:09:01 -0400

    Мой коммит через токен

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Добро пожаловать в BioMath Core!</h1>
+      <p>Теперь ты управляешь сайтом сам!</p>
+    </main>
+  )
+}
